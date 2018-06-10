000b549b28a89f7f600ab45aa5cfc9b5b566f092
mikolajmichta <hansolo92@wp.pl>
2018-06-11 01:38:19 +0200

    added Corgi functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,11 @@
-/* Smooth scrolling */
+/* Bootstrap tooltips */
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+/* Smooth scrolling on links*/
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -10,3 +17,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+/* Make menu vertical on small screen */
+$(window).resize(function () {
+    if ($(window).width() < 420) {
+        $('#mainNavBar').removeClass('btn-group');
+        $('#mainNavBar').addClass('btn-group-vertical');
+    } else {
+        $('#mainNavBar').addClass('btn-group');
+        $('#mainNavBar').removeClass('btn-group-vertical');
+    }
+});
+
+
+/* Img replacement */
+
+async function getCorgi() {
+    let response = await fetch(`https://dog.ceo/api/breed/corgi/cardigan/images/random`);
+    let jsonCorgi = await response.json();
+    return jsonCorgi.message;
+}
+
+async function replaceImg() {
+    $('.img-fluid').each(async function () {
+        let doggo = await getCorgi()
+        $(this).attr(`src`, doggo)
+    })
+}
+
+$(`#corgi-head`).on(`click`, replaceImg);
